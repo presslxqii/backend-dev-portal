fe6323b9ef0d857589b1f9d8b1b81c74925f8cff
Maksim Berdnikov <presslxqii@yandex.ru>
2022-08-31 19:19:14 +0300

    something

--- src/lib/asyncHandler.ts
+++ src/lib/asyncHandler.ts
@@ -1,8 +1,8 @@
-import * as express from 'express';
-import * as P from 'bluebird';
-import log, {TDebug} from '../log';
+import * as express from "express";
+import * as P from "bluebird";
+import log, {TDebug} from "../log";
 
-const debug = new TDebug('src:lib:asyncHandler');
+const debug = new TDebug("src:lib:asyncHandler");
 
 export interface HandlerOption {
     cache?: boolean;
@@ -12,10 +12,10 @@ export interface HandlerOption {
 export function asyncHandler(
     handler: (req: express.Request, res: express.Response, next) => P<any>,
     name: string, options?: HandlerOption): express.Handler {
-    debug.log('Register handler with option: %o', options);
+    debug.log("Register handler with option: %o", options);
     return (req: express.Request, res: express.Response, next) => {
         async function exec(): P<any> {
-            debug.start('SERVICE:' + name);
+            debug.start("SERVICE:" + name);
             if (options && options.cache) {
                 try {
                     const item = await getCache(req);
@@ -26,29 +26,29 @@ export function asyncHandler(
                     log.error(e);
                     console.error(e.stack);
                 }
-                
+
             }
             const data = await handler(req, res, next);
             if (data && options && options.cache) {
                 cache(req, {
                     data,
-                    expTs: Date.now() + options.cacheLive ? options.cacheLive : 3600000,
+                    expTs: Date.now() + options.cacheLive ? options.cacheLive : 3600000
                 });
             }
             return data;
         }
-        
+
         exec()
             .then((data) => {
-                debug.end('SERVICE:' + name);
+                debug.end("SERVICE:" + name);
                 if (data) {
                     res.json(data);
                 } else if (!res.finished) {
-                    debug.log('no more response to send, status code: %d', res.statusCode);
+                    debug.log("no more response to send, status code: %d", res.statusCode);
                     res.end();
                 }
             }, (error) => {
-                debug.end('SERVICE:' + name);
+                debug.end("SERVICE:" + name);
                 next(error);
             });
     };
@@ -64,24 +64,24 @@ const cachedData: { [key: string]: CachedItem } = {};
 // TODO use cache server
 async function cache(req: express.Request, item: CachedItem): P<any> {
     const fullUrl = getUrl(req);
-    debug.log('Set cache: %s data: %o', fullUrl, item);
+    debug.log("Set cache: %s data: %o", fullUrl, item);
     cachedData[fullUrl] = item;
 }
 
 async function getCache(req: express.Request): P<CachedItem> {
     const fullUrl = getUrl(req);
-    debug.log('Get cache: %s', fullUrl);
+    debug.log("Get cache: %s", fullUrl);
     const data = cachedData[fullUrl];
     if (data && data.expTs < Date.now()) {
-        debug.log('Cache expired: %s', fullUrl);
+        debug.log("Cache expired: %s", fullUrl);
         delete cachedData[fullUrl];
         return;
     } else {
-        debug.log('Hit cache: %s', fullUrl);
+        debug.log("Hit cache: %s", fullUrl);
         return data;
     }
 }
 
 function getUrl(req: express.Request) {
-    return req.protocol + '://' + req.get('host') + req.originalUrl;
+    return req.protocol + "://" + req.get("host") + req.originalUrl;
 }
